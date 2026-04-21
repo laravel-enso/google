55c7d99765ee5a07ed8d26620e823866abcd021f
Manuela Mindroc <manuelamindroc@MacBook-Pro-Manuela-2.local>
2026-04-21 15:46:55 +0300

    fixes upgrade

--- src/Upgrades/AddBackGoogleTableColumns.php
+++ src/Upgrades/AddBackGoogleTableColumns.php
@@ -3,12 +3,15 @@
 namespace LaravelEnso\Google\Upgrades;
 
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
+use LaravelEnso\Google\Models\Settings;
+use LaravelEnso\Upgrade\Contracts\MigratesData;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
 use LaravelEnso\Upgrade\Contracts\ShouldRunManually;
 use LaravelEnso\Upgrade\Helpers\Table;
 
-class AddBackGoogleTableColumns implements MigratesTable, ShouldRunManually
+class AddBackGoogleTableColumns implements MigratesTable, MigratesData, ShouldRunManually
 {
     public function isMigrated(): bool
     {
@@ -28,5 +31,20 @@ class AddBackGoogleTableColumns implements MigratesTable, ShouldRunManually
             $table->string('recaptcha_secret', 300)->nullable();
             $table->string('tag_manager_id')->nullable();
         });
+    }
+
+    public function migrateData(): void
+    {
+        Settings::current()->update([
+            'analytics_id' => Config::get('enso.google.analyticsId'),
+            'ads_id' => Config::get('enso.google.adsId'),
+            'place_id' => Config::get('enso.google.placeId'),
+            'maps_key' => Config::get('enso.google.mapsKey'),
+            'geocoding_key' => Config::get('enso.google.geocodingKey'),
+            'places_key' => Config::get('enso.google.placesKey'),
+            'recaptcha_key' => Config::get('enso.google.recaptchaKey'),
+            'recaptcha_secret' => Config::get('enso.google.recaptchaSecret'),
+            'tag_manager_id' => Config::get('enso.google.tagManagerId'),
+        ]);
     }
 }
